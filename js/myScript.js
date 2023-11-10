34be1c1764bafe3363f3a3dd10f085e521b1a626
Vlada-WoodyChick <uchusa@mail.ru>
2023-11-10 13:20:13 +0300

    function oddOreEven

--- js/myScript.js
+++ js/myScript.js
@@ -1,2 +1,11 @@
- 
-   
+function oddOreEven(a) {
+    if (a % 2) {
+        document.getElementById(`demo1`).innerHTML = a + " " + "нечетное"
+        return;
+    };
+    if (!(a % 2)) { document.getElementById(`demo2`).innerHTML = a + " " + "четное" };
+    return
+
+}
+oddOreEven(6);
+oddOreEven(9);
